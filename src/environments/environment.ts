2f282c57e63fe8fe31363c759bd4eb7c2a8e8cca
krystian john dumapit <23546777+Kryuzaki18@users.noreply.github.com>
2026-04-02 18:16:27 +0800

    fix: api base url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,5 @@
 export const environment = {
   binanceFutureRestBaseUrl: 'https://testnet.binancefuture.com',
-  binanceFutureWebSocketBaseUrl: 'https://testnet.binancefuture.com/fapi/v1/order',
-  binanceSpotRestBaseUrl: 'https://testnet.binance.vision',
-  binanceSpotWebSocketBaseUrl: 'wss://testnet.binance.vision/ws',
+  binanceFutureWebSocketBaseUrl: 'wss://stream.binancefuture.com/ws',
   apiTradingBotUrl: 'http://127.0.0.1:3000'
 };
