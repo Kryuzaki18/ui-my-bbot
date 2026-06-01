a3d22bd78242cd68cb8fd6495c4be1cbbf38bc44
krystian john dumapit <23546777+Kryuzaki18@users.noreply.github.com>
2026-06-02 02:58:42 +0800

    chore: update proxy

--- netlify/edge-functions/fapi-proxy.ts
+++ netlify/edge-functions/fapi-proxy.ts
@@ -4,14 +4,15 @@ export default async (request: Request) => {
 
   return fetch(binanceUrl, {
     headers: {
-      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-      "Accept": "application/json, text/plain, */*",
-      "Accept-Language": "en-US,en;q=0.9",
-      "Accept-Encoding": "gzip, deflate, br",
-      "Referer": "https://www.binance.com",
-      "Origin": "https://www.binance.com",
-    }
+      'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+      Accept: 'application/json, text/plain, */*',
+      'Accept-Language': 'en-US,en;q=0.9',
+      'Accept-Encoding': 'gzip, deflate, br',
+      Referer: 'https://www.binance.com',
+      Origin: 'https://www.binance.com',
+    },
   });
 };
 
-export const config = { path: "/fapi/*" };
+export const config = { path: '/fapi/*' };
